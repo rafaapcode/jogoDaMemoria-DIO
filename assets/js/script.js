322a02de22d930847bb9e5313ece133d655d4454
rafaapcode <rafaap2003@gmail.com>
2022-06-08 13:17:23 -0300

    Mensagem Final e Recarregar o Jogo

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ const cards = document.querySelectorAll(".card");
 let hasflipped = false;
 let firstCard, secondCard;
 let lockBoard = false;
+let cardsFliped = 0;
+const mensagemFinal = document.querySelector(".memory-game h1");
 
 function flipCard() {
     if (lockBoard) return;
@@ -24,12 +26,36 @@ function flipCard() {
 
 function checkForMath() {
     if (firstCard.dataset.card === secondCard.dataset.card) {
+        fimDeJogo();
         disableCards();
         return;
     }
 
     unflipCard();
 }
+
+function fimDeJogo() {
+    if (firstCard.dataset.card === secondCard.dataset.card) {
+        cardsFliped = cardsFliped + 1;
+
+        reloadShowFinalMessage(cardsFliped);
+
+    }
+}
+
+function reloadShowFinalMessage(cardsFliped) {
+    if (cardsFliped === 6) {
+        setTimeout(() => {
+
+            mensagemFinal.style.display = 'block';
+
+            setTimeout(() => {
+                location.reload();
+            }, 1500);
+        }, 1000);
+    }
+}
+
 
 function disableCards() {
     firstCard.removeEventListener("click", flipCard);
@@ -46,13 +72,20 @@ function unflipCard() {
         secondCard.classList.remove("flip");
 
         resetBoard();
-    }, 1500);
+    }, 1000);
 }
 
 function resetBoard() {
     [hasflipped, lockBoard] = [false, false];
     [firstCard, secondCard] = [null, null];
 }
+
+(function shuffle() {
+    cards.forEach((card) => {
+        let randomPosition = Math.floor(Math.random() * 12);
+        card.style.order = randomPosition;
+    });
+})();
 
 cards.forEach(card => {
     card.addEventListener("click", flipCard);
